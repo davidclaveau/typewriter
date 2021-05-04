a56dcd0184882f0229c2f0a9a29ef7bba4242543
David Claveau <daveclaveau@gmail.com>
2021-05-04 19:06:33 +0000

    Added  typwriter.js which takes a string and returns each character of the string as an animation on one line.

---
+++ typewriter.js
@@ -0,0 +1,14 @@
+const sentence = "hello there from lighthouse labs";
+let time = 50;
+
+for (const char of sentence) {
+time += 50;
+
+setTimeout(() => {
+    process.stdout.write(char);
+  }, time)
+}
+
+setTimeout(() => {
+ console.log("\n")
+}, time)
